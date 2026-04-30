c741f36b702ecb110e94fc2c224591604d2c3f88
emmanuellabanfro3-design <emmanuellabanfro3@gmail.com>
2026-04-30 14:36:33 -0700

    Update Banfro_Emmanuella_6308124.f90

--- MET253_Assignment_repo/Banfro_Emmanuella_6308124.f90
+++ MET253_Assignment_repo/Banfro_Emmanuella_6308124.f90
@@ -1,30 +1,28 @@
 PROGRAM midsem             !6308124 = INDEX NUMBER
 IMPLICIT NONE
-CHARACTER(LEN=15) :: DISTINCTION,CREDIT,PASS,FAIL
 INTEGER,DIMENSION(10) :: a
-a(1)=85
-a(2)=62
-a(3)=45
-a(4)=91
-a(5)=38
-a(6)=74
-a(7)=55
-a(8)=88
-a(9)=61
-a(10)=47
-PRINT*,a
-IF(100<=a<=80),THEN
-PRINT*, A,DISTINCTION
-ELSE
-IF(60<=a<=79),THEN
-PRINT*, B,CREDIT
-ELSE
-IF(40<=a<=59),THEN
-PRINT*, C,PASS
-ELSE
-IF(0<=a<=39),THEN
-PRINT*, D,FAIL
+INTEGER :: i
+CHARACTER(LEN=1) :: grade
+CHARACTER(LEN=15) :: remark
+a=(/85,62,45,91,38,74,55,88,61,47/)
+PRINT*,
+PRINT*,'STUDENT     SCORE     GRADE     REMARK'
+PRINT*,
+Do i = 1,10
+IF(a(i)>=80.AND. a(i) <=100),THEN
+grade = 'A'
+remark = 'Distinction'
+ELSE IF(a(i)>=60.AND. a(i) <=79),THEN
+grade = 'B'
+remark = 'Credit'
+ELSE IF(a(i)>=40.AND. a(i) <=59),THEN
+grade = 'C'
+remark = 'Pass'
+ELSE IF(a(i)>=0.AND. a(i) <=39),THEN
+grade = 'F'
+remark = 'Fail'
 END IF
+END DO
 END PROGRAM midsem
 
  
